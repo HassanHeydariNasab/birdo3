0004e02aae2a6e9d302c71eabf314c3573fdc871
hsn6 <hsn6@openmailbox.org>
2018-05-10 20:37:08 +0430

    Remove tests

--- test.gd
+++ test.gd
@@ -1,11 +1,19 @@
 extends Spatial
 
-onready var a = get_node("animation_player")
-
 func _ready():
-	a.play("anim")
 	set_process(true)
 
 func _process(delta):
-	if Input.is_action_just_pressed("flugi"):
-		a.play("anim")
+	if Input.is_action_pressed('ui_up'):
+		$B.translate(Vector3(0,0.1,0))
+	if Input.is_action_pressed('ui_down'):
+		$B.translate(Vector3(0,-0.1,0))
+	if Input.is_action_pressed('ui_left'):
+		$B.translate(Vector3(-0.1,0,0))
+	if Input.is_action_pressed('ui_right'):
+		$B.translate(Vector3(0.1,0,0))
+	if Input.is_action_pressed('ui_page_down'):
+		$B.translate(Vector3(0,0,0.1))
+	if Input.is_action_pressed('ui_page_up'):
+		$B.translate(Vector3(0,0,-0.1))
+	$A.look_at($B.get_global_transform().origin, Vector3(-1,0,0))
